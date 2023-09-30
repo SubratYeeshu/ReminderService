afb4154362e014cdab02935b4e3525698f1a4241
SubratYeeshu <shubrat.parth@gmail.com>
2023-09-30 18:27:03 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ const { PORT } = require('./config/serverConfig');
 
 const {sendBasicEmail} = require('./services/email-service');
 
+const cron = require('node-cron');
+
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-    
+
+
     app.listen(PORT, () => {
         console.log(`Server listening on port ${PORT}`);
 
@@ -18,6 +21,11 @@ const setupAndStartServer = () => {
         //     'Test Email',
         //     'Hello World from SMTP server!'
         //     );
+
+        // cron.schedule( '*/2 * * * * *', () => {
+        //     console.log('Running Cron Job');
+        // })
+
     });
 }
 
