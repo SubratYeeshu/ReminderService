7c9b794858f56e8cb59a747a71880ba258a5b3e4
SubratYeeshu <shubrat.parth@gmail.com>
2023-10-01 21:21:46 +0530

    Fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const { PORT } = require('./config/serverConfig');
 
-const {sendBasicEmail} = require('./services/email-service');
-
 const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service');
 
 const jobs = require('./utils/job');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-
+    app.use(bodyParser.urlencoded({extended: true}));
 
     app.post('/api/v1/tickets', TicketController.create);
 
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+
     app.listen(PORT, () => {
-        console.log(`Server listening on port ${PORT}`);
-
-        jobs();
+        console.log(`Server started at port ${PORT}`);
         // jobs();
-        // sendBasicEmail(
-        //     'support@admin.com',
-        //     'shubrat.parth@gmail.com',
-        //     'Test Email',
-        //     'Hello World from SMTP server!'
-        //     );
-
-        // cron.schedule( '*/2 * * * * *', () => {
-        //     console.log('Running Cron Job');
-        // })
-
+        
     });
 }
 
 setupAndStartServer();
+
+
+// amqplib library is establishing a connection with RabbitMQ server using amqp protocol.
--- src/utils/job.js
+++ src/utils/job.js
@@ -37,3 +37,16 @@ const setupJobs = (timestamp) => {
 }
 
 module.exports = setupJobs;
+
+
+/*
+
+
+Service 1 (100Mbps) -> Publisher -----> Message Queue [msg1, msg2, msg3, msg4, msg5, msg6, msg7, msg8, msg9, msg10] -> Service 2 (20 Mbps)
+
+Service 1: Booking Service
+Service 2: Notification / Reminder Service
+To resolve bottle neck, we will use message queue
+because we dont need immediate response from service 2, we can wait for some time to get the response from service 2
+
+*/
